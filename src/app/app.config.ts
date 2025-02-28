bf690a02a86750d6aaa10a56e6e2e1ba83d948d2
LECCESE <pablolec@hotmail.com>
2025-02-28 11:24:49 -0300

    CreatedGenerosService

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,6 +5,7 @@ import { provideMomentDateAdapter } from '@angular/material-moment-adapter';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -22,7 +23,8 @@ export const appConfig: ApplicationConfig = {
       dateA11yLabel: 'LL',
       monthYearA11yLabel: 'MMMM YYYY'
     }
-  })
+  }),
+  provideHttpClient(withFetch())
 ]
 };
 
--- src/app/generos/indice-generos/indice-generos.component.ts
+++ src/app/generos/indice-generos/indice-generos.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
+import { GenerosService } from '../generos.service';
+import { environment } from '../../../environments/environment.development';
 
 @Component({
   selector: 'app-indice-generos',
@@ -9,5 +11,15 @@ import { RouterLink } from '@angular/router';
   styleUrl: './indice-generos.component.css'
 })
 export class IndiceGenerosComponent {
+
+  //Inyección de servicio
+  generosService = inject(GenerosService)
+
+  constructor(){
+    this.generosService.obtenerTodos().subscribe(generos => {
+      console.log(generos);
+    })
+  }
+
 
 }
